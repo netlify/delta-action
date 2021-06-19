31bcbd7f06b5c940d77b48cd776a8904ceba4047
Eduardo Bouças <mail@eduardoboucas.com>
2021-06-19 10:34:25 +0100

    fix: account for missing metrics in the base branch

--- src/lib/comment.js
+++ src/lib/comment.js
@@ -14,23 +14,24 @@ const createHeadBranchComment = ({ commitSha, metrics, job, previousCommit, titl
   return `## ${title}\n\n${metricsList}\n${metadata}`
 }
 
-const createPullRequestComment = ({ baseSha, job, metrics, previousMetrics = {}, title }) => {
+const createPullRequestComment = ({ baseSha, job, metrics, previousMetrics, title }) => {
+  // Accounting for both the legacy metadata format (object) and the new
+  // format (array of objects).
+  const previousMetricsArray = (Array.isArray(previousMetrics) ? previousMetrics : [previousMetrics]).filter(Boolean)
   const metadata = `<!--delta:${job}@{}-->`
   const metricsList = metrics
     .map((metric) => {
-      // Accounting for both the legacy metadata format (object) and the new
-      // format (array of objects).
-      const comparison = Array.isArray(previousMetrics) ? previousMetrics[0] : previousMetrics
+      const [comparison = {}] = previousMetricsArray
       const previousValue = comparison[metric.name]
       // eslint-disable-next-line dot-notation
       const previousSha = comparison['__commit']
-      const graphMetrics = [...previousMetrics, { __commit: baseSha, [metric.name]: metric.value }]
+      const graphMetrics = [...previousMetricsArray, { __commit: baseSha, [metric.name]: metric.value }]
       const graph = getGraph({ metrics: graphMetrics, metricName: metric.name, units: metric.units })
 
       return getMetricLine(metric, previousValue, previousSha, graph)
     })
     .join('\n')
-  const baseShaLine = baseSha && previousMetrics.length !== 0 ? `*Comparing with ${baseSha}*\n\n` : ''
+  const baseShaLine = baseSha && previousMetricsArray.length !== 0 ? `*Comparing with ${baseSha}*\n\n` : ''
 
   return `## ${title}\n\n${baseShaLine}${metricsList}\n${metadata}`
 }
