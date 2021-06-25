3355e623c1fa950be85ef4cdd843c516e7d96688
Eduardo Bouças <mail@eduardoboucas.com>
2021-06-25 16:41:59 +0100

    feat: draw mean in graph

--- src/lib/graph.js
+++ src/lib/graph.js
@@ -1,6 +1,9 @@
 const BAR_BODY = '|  |'
 const BAR_BODY_FILLED = '|▒▒|'
+const BAR_BODY_MEAN = '┼──┼'
 const BAR_TOP = '┌──┐'
+const GAP = ' '
+const GAP_MEAN = '─'
 const GAP_LENGTH = 4
 const LINE_COUNT = 20
 
@@ -18,9 +21,12 @@ const drawBase = (points) => {
   return `└─${axis}>\n  ${legend}`
 }
 
-const drawGraph = (values, { fillLast = false } = {}) => {
+const drawGraph = (values, { drawMean = false, fillLast = false } = {}) => {
   const maxValue = values.reduce((max, { value }) => (value > max ? value : max), 0)
+  const sum = values.reduce((acc, { value }) => acc + value, 0)
+  const mean = sum / values.length
   const increment = maxValue / LINE_COUNT
+  const meanLevel = drawMean ? LINE_COUNT - Math.round(mean / increment) : null
   const augmentedValues = values.map((dataPoint) => {
     const filledLevels = Math.round(dataPoint.value / increment)
 
@@ -28,7 +34,7 @@ const drawGraph = (values, { fillLast = false } = {}) => {
   })
 
   const levels = Array.from({ length: LINE_COUNT }, (_, index) =>
-    drawLevel({ fillLast, level: index + 1, values: augmentedValues }),
+    drawLevel({ fillLast, isMean: meanLevel === index + 1, level: index + 1, values: augmentedValues }),
   )
   const topLevels = [
     drawLevel({ level: -1, values: augmentedValues }),
@@ -39,15 +45,18 @@ const drawGraph = (values, { fillLast = false } = {}) => {
   return `${[...topLevels, ...levels].join('\n')}\n${base}`
 }
 
-const drawLevel = ({ fillLast, level, values }) => {
-  const padding = ' '.repeat(GAP_LENGTH / 2)
+const drawLevel = ({ fillLast, isMean, level, values }) => {
+  const gapCharacter = isMean ? GAP_MEAN : GAP
+  const padding = gapCharacter.repeat(GAP_LENGTH / 2)
   const bars = values
     // eslint-disable-next-line complexity
     .map(({ displayValue, emptyLevels, value }, index) => {
       const isLastValue = index === values.length - 1
 
       if (emptyLevels < level) {
-        return `${padding}${isLastValue && fillLast ? BAR_BODY_FILLED : BAR_BODY}${padding}`
+        const unfilledBody = isMean ? BAR_BODY_MEAN : BAR_BODY
+
+        return `${padding}${isLastValue && fillLast ? BAR_BODY_FILLED : unfilledBody}${padding}`
       }
 
       if (emptyLevels === level) {
@@ -55,17 +64,17 @@ const drawLevel = ({ fillLast, level, values }) => {
       }
 
       if (emptyLevels - 1 === level) {
-        return getPaddedString((displayValue || value).toString(), BAR_BODY.length + GAP_LENGTH)
+        return getPaddedString((displayValue || value).toString(), BAR_BODY.length + GAP_LENGTH, gapCharacter)
       }
 
-      return `${padding}${' '.repeat(BAR_BODY.length)}${padding}`
+      return `${padding}${gapCharacter.repeat(BAR_BODY.length)}${padding}`
     })
     .join('')
 
   return `${level === -1 ? '^' : '│'} ${bars}`
 }
 
-const getPaddedString = (string, length) => {
+const getPaddedString = (string, length, paddingCharacter = ' ') => {
   const totalPadding = length - string.length
 
   if (totalPadding < 0) {
@@ -74,8 +83,8 @@ const getPaddedString = (string, length) => {
 
   const paddingRightLength = Math.max(0, Math.round(totalPadding / 2))
   const paddingLeftLength = Math.max(0, totalPadding - paddingRightLength)
-  const paddingLeft = ' '.repeat(paddingLeftLength)
-  const paddingRight = ' '.repeat(paddingRightLength)
+  const paddingLeft = paddingCharacter.repeat(paddingLeftLength)
+  const paddingRight = paddingCharacter.repeat(paddingRightLength)
 
   return `${paddingLeft}${string}${paddingRight}`
 }
