77b41839f7eb8486282ba762bf4f0a1bd801e39e
Eduardo Bouças <mail@eduardoboucas.com>
2022-03-07 15:26:45 +0000

    fix: output ESM (#266)
    * fix: add shims for CJS built-ins
    
    * refactor: use ESM

---
+++ scripts/build.js
@@ -0,0 +1,22 @@
+import { buildSync } from 'esbuild'
+
+const jsBanner = `
+import { createRequire as ___internalCreateRequire } from 'module';
+import { fileURLToPath as ___internalFileURLToPath } from "url";
+import { dirname as ___internalPathDirname} from "path";
+let __filename =___internalFileURLToPath(import.meta.url);
+let __dirname = ___internalPathDirname(___internalFileURLToPath(import.meta.url));
+let require = ___internalCreateRequire(import.meta.url);
+`
+
+buildSync({
+  banner: {
+    js: jsBanner,
+  },
+  bundle: true,
+  entryPoints: ['src/index.js'],
+  format: 'esm',
+  platform: 'node',
+  outdir: 'dist',
+  sourcemap: true,
+})
